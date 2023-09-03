076a1a1d6da5a80ed4868b1f455719006582626c
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-09-03 16:38:36 -0500

    add search bar and search button in search page

--- src/pages/Searchpage.js
+++ src/pages/Searchpage.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react"; // Imports React libraries as well as useEffect and useState
+import Auth from "../utils/auth";
+import { useMutation, useQuery } from "@apollo/client";
+
 
 const Searchpage = () => {
+
+    const [company, setCompany] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleCompanyChange = (event) => {
+        setCompany(event.target.value);
+    };
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(company);
+        setCompany([]);
+    };
     return (
-        <div>
-        <h3> This is SEARCH TEST</h3> 
+        <div className= "search-companies">
+            <div className="search-bar">
+                <form className="search-form" onSubmit={handleFormSubmit}>
+                    <input
+                       id="search"
+                       type="text"
+                       value={company}
+                       onChange={handleCompanyChange}
+                        placeholder="Search for a company"
+                    />
+                    <button type="submit">Search</button>
+                    
+                </form>
+                    
+            </div>
+
+
+         
 
         </div>
     );
