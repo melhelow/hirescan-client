624c8fe84aeb77373dea084a6349844317d98ebd
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-09-03 20:01:18 -0500

    rendered searchpage to my profile else login

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Auth from '../utils/auth';
 
-const Profile = () => {
+import Searchpage from '../pages/Searchpage';
+
+export default function Profile () {
+
+if(Auth.loggedIn()) {
     return (
         <div>
-            
+            <h1>My Profile</h1>
+            <Searchpage/>
         </div>
-    );
-};
 
-export default Profile;
+    );
+}
+else {
+    window.location.assign('/login');
+}
+}
+
